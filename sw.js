2aae4fa167d13fb1149b7d99fa9f4687c936e093
masakitokugawa51-create <masakitokugawa51@gmail.com>
2026-02-25 03:58:00 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'btc-ngn-v1';
+const CACHE_NAME = 'btc-ngn-v2';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
 self.addEventListener('install', (e) => {
@@ -28,7 +28,7 @@ self.addEventListener('fetch', (e) => {
     );
   } else {
     e.respondWith(
-      caches.match(e.request).then((cached) => cached || fetch(e.request))
+      fetch(e.request).catch(() => caches.match(e.request))
     );
   }
 });
